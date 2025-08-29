10875dff2cd11e5ba2cf31299e0f84f8da1fa9eb
Divakar Rajesh S <divakar@sensara.tv>
2025-08-30 00:24:57 +0800

    unlocking state

--- packages/lynx-project/src/DetailView.tsx
+++ packages/lynx-project/src/DetailView.tsx
@@ -12,9 +12,9 @@ interface DetailParams {
 
 export function DetailView({ picture: pic, onClose }: DetailParams) {
   const [picture, setPicture] = useState<Photo>(pic);
+  const [isUnlocking, setIsUnlocking] = useState(true);
   const [showImage, setShowImage] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
-  const [pinInput, setPinInput] = useState("");
   const [error, setShowError] = useState(false);
 
   const unlock = async () => {
@@ -24,9 +24,11 @@ export function DetailView({ picture: pic, onClose }: DetailParams) {
       setShowError(false);
       setShowPopup(false);
       setShowImage(true);
+      setIsUnlocking(true);
       let newPicture = await NativeService.unlockImage(picture);
       console.log(newPicture)
       setPicture(newPicture);
+      setIsUnlocking(false);
     } else {
       setShowError(true);
     }
@@ -111,8 +113,11 @@ export function DetailView({ picture: pic, onClose }: DetailParams) {
             //   setPinInput(res.detail.value);
             // }}
             />
-            <view className="unlock-button" bindtap={() => unlock(pinInput)}>
-              <text className="unlock-button-text">Unlock</text>
+            <view className={`unlock-button ${isUnlocking ? "is-unlocking" : ""}`} bindtap={() => !isUnlocking && unlock()}>
+              {isUnlocking && <view className="loader small" style={{ marginRight: "8px" }} />}
+              <text className="unlock-button-text">{
+                isUnlocking ? "Unlocking..." : "Unlock"
+              }</text>
             </view>
             {error && <view style={{ marginTop: "10px" }}>
               <text style={{ color: "red", textAlign: "center" }}>Incorrect PIN. Please try again.</text>
